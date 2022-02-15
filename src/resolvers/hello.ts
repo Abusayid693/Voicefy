3d47f27469b1f2171085c88973736e0567474e49
Rehane <abusayid693@gmail.com>
2022-02-15 18:25:17 +0530

    feat : Seperated client and server

--- src/resolvers/hello.ts
+++
@@ -1,9 +0,0 @@
-import { Resolver, Query, Arg } from "type-graphql";
-
-@Resolver()
-export class HelloResolver {
-  @Query(() => String)
-  hello(@Arg("options") options: string): String {
-    return options + "bye";
-  }
-}
--- src/types.ts
+++
@@ -1,8 +0,0 @@
-import { Connection, IDatabaseDriver, EntityManager } from "@mikro-orm/core";
-import { Request, Response } from "express";
-
-export type MyContext = {
-  em: EntityManager<IDatabaseDriver<Connection>>;
-  req: Request ;
-  res: Response;
-};
