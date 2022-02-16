a7166e6e3c426431b956f7b53467596c407c93b3
Rehane <abusayid693@gmail.com>
2022-02-17 05:01:04 +0530

    fix : jwt

--- helpers/index.ts
+++ helpers/index.ts
@@ -1,8 +1,8 @@
-import crypto from "crypto";
 import jwt from "jsonwebtoken";
+import { JWT_SECRET, JWT_EXPIRE } from "../local.config";
 
 export const generateToken = ({ _id }: { _id: any }) => {
-  return jwt.sign({ id: _id }, "env.JWT_SECRET", {
-    expiresIn: "1000min",
+  return jwt.sign({ id: _id }, JWT_SECRET, {
+    expiresIn: JWT_EXPIRE,
   });
 };
